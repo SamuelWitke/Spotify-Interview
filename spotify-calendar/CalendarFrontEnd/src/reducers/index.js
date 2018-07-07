3d33a0524b04e4c839c01c5c7ce744ea0452418b
SamuelWitke <SamuelWitke@gmail.com>
2018-07-07 09:29:32 -0400

    Full Stack

---
+++ spotify-calendar/CalendarFrontEnd/src/reducers/index.js
@@ -0,0 +1,9 @@
+import user from './users.js';
+import { combineReducers } from 'redux'
+import {reducer as notifications} from 'react-notification-system-redux';
+
+
+export default combineReducers({
+	userReducer: user,
+	notifications
+	})
